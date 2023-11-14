f76de99b3e36c9921b7749fb987131d03e62f008
Savva Bakin <savva2005bakin@gmail.com>
2023-11-14 19:46:36 +0300

    fix
    fixed: function_graph.py, japan_flag.py, pattern.py

--- japan_flag.py
+++ japan_flag.py
@@ -1,46 +1,46 @@
 import sys
+ROW1_L = 8
+ROW2_L = 10
+ROW3_L = 12
+STEP_BW_ROWS = 1
+WIDTH = 11
+LENGTH = 41
+
+
+def line_in_circle(row, scaleup=True):
+    sys.stdout.write('\u001b[1A')
+    if scaleup:
+        sys.stdout.write(f'\u001b[{str(row - STEP_BW_ROWS)}D')
+    else:
+        sys.stdout.write(f'\u001b[{str(row + STEP_BW_ROWS)}D')
+    sys.stdout.write('\u001b[41;1m ' * row)
 
 
 def japan_flag():
-    
-    row1_l = 8
-    row2_l = 10
-    row3_l = 12
-    step_bw_rows = 1
-    width = 11
-    length = 41
 
     #creates white rectangle
-    for i in range(width):
-        print(u'\u001b[47;1m ' * length, u'\u001b[0m')
+    for i in range(WIDTH):
+        print('\u001b[47;1m ' * LENGTH, '\u001b[0m')
 
     #creates first(bottom) row of the red circle
-    sys.stdout.write(u'\u001b[' + str(width//2 - 1) + 'A')
-    sys.stdout.write(u'\u001b[' + str(length//2 - (row1_l//2 - 1)) + 'C')
-    sys.stdout.write(u'\u001b[41;1m ' * row1_l)
+    sys.stdout.write(f'\u001b[{str(WIDTH//2 - 1)}A')
+    sys.stdout.write(f'\u001b[{str(LENGTH//2 - (ROW1_L//2 - 1))}C')
+    sys.stdout.write('\u001b[41;1m ' * ROW1_L)
 
     #creates second row of the red circle
-    sys.stdout.write(u'\u001b[1A')
-    sys.stdout.write(u'\u001b[' + str(row2_l - step_bw_rows) + 'D')
-    sys.stdout.write(u'\u001b[41;1m ' * row2_l)
+    line_in_circle(ROW2_L)
 
     #creates third row of the red circle
-    sys.stdout.write(u'\u001b[1A')
-    sys.stdout.write(u'\u001b[' + str(row3_l - step_bw_rows) + 'D')
-    sys.stdout.write(u'\u001b[41;1m ' * row3_l)
+    line_in_circle(ROW3_L)
 
     #creates fourth row of the red circle
-    sys.stdout.write(u'\u001b[1A')
-    sys.stdout.write(u'\u001b[' + str(row2_l + step_bw_rows) + 'D')
-    sys.stdout.write(u'\u001b[41;1m ' * row2_l)
+    line_in_circle(ROW2_L, scaleup=False)
 
     #creates fifth row of the red circle
-    sys.stdout.write(u'\u001b[1A')
-    sys.stdout.write(u'\u001b[' + str(row1_l + step_bw_rows) + 'D')
-    sys.stdout.write(u'\u001b[41;1m ' * row1_l)
+    line_in_circle(ROW1_L, scaleup=False)
 
     #resets terminal settings
-    sys.stdout.write(u'\u001b[' + str(width) + 'B')
-    sys.stdout.write(u'\u001b[0m')
+    sys.stdout.write(f'\u001b[{str(WIDTH)}B')
+    sys.stdout.write('\u001b[0m')
 
 japan_flag()
--- pattern.py
+++ pattern.py
@@ -1,62 +1,62 @@
 import sys
 
 
+WIDTH = 11
+LENGTH = 24
+
 def pattern():
 
-    width = 11
-    length = 24
-
     #creates white rectangle
-    for i in range(width):
-        print(u'\u001b[47;1m ' * length, u'\u001b[0m')
+    for i in range(WIDTH):
+        print('\u001b[47;1m ' * LENGTH, '\u001b[0m')
 
     #creates bottom half of the pattern 
-    for i in range(0, width//2 + 1):
+    for i in range(0, WIDTH//2 + 1):
         #creates left half
-        sys.stdout.write(u'\u001b[' + str(length//4 + i) + 'C')
-        sys.stdout.write(u'\u001b[40;1m ')
-        sys.stdout.write(u'\u001b[' + str(length//4 + i) + 'D')
-        sys.stdout.write(u'\u001b[' + str(length//4 - i) + 'C')
-        sys.stdout.write(u'\u001b[40;1m ')
-        sys.stdout.write(u'\u001b[' + str(length//4 - i) + 'D')
+        sys.stdout.write(f'\u001b[{str(LENGTH//4 + i)}C')
+        sys.stdout.write('\u001b[40;1m ')
+        sys.stdout.write(f'\u001b[{str(LENGTH//4 + i)}D')
+        sys.stdout.write(f'\u001b[{str(LENGTH//4 - i)}C')
+        sys.stdout.write('\u001b[40;1m ')
+        sys.stdout.write(f'\u001b[{str(LENGTH//4 - i)}D')
 
         #creates right half
-        sys.stdout.write(u'\u001b[' + str((length//4)*3 + i - 4) + 'C')
-        sys.stdout.write(u'\u001b[40;1m ')
-        sys.stdout.write(u'\u001b[' + str((length//4)*3 + i - 4) + 'D')
-        sys.stdout.write(u'\u001b[' + str((length//4)*3 - i - 4) + 'C')
-        sys.stdout.write(u'\u001b[40;1m ')
+        sys.stdout.write(f'\u001b[{str((LENGTH//4)*3 + i - 4)}C')
+        sys.stdout.write('\u001b[40;1m ')
+        sys.stdout.write(f'\u001b[{str((LENGTH//4)*3 + i - 4)}D')
+        sys.stdout.write(f'\u001b[{str((LENGTH//4)*3 - i - 4)}C')
+        sys.stdout.write('\u001b[40;1m ')
 
         #sets new row
-        sys.stdout.write(u'\u001b[1A')
-        sys.stdout.write(u'\u001b[100D')
+        sys.stdout.write('\u001b[1A')
+        sys.stdout.write('\u001b[100D')
 
     #move cursor up
-    sys.stdout.write(u'\u001b[' + str(width - 6) + 'A')
+    sys.stdout.write(f'\u001b[{str(WIDTH - 6)}A')
 
     #creates upper part of the pattern
-    for i in range(0, width//2 + 1):
+    for i in range(0, WIDTH//2 + 1):
         #creates left half
-        sys.stdout.write(u'\u001b[' + str(length//4 + i) + 'C')
-        sys.stdout.write(u'\u001b[40;1m ')
-        sys.stdout.write(u'\u001b[' + str(length//4 + i) + 'D')
-        sys.stdout.write(u'\u001b[' + str(length//4 - i) + 'C')
-        sys.stdout.write(u'\u001b[40;1m ')
-        sys.stdout.write(u'\u001b[' + str(length//4 - i) + 'D')
+        sys.stdout.write(f'\u001b[{str(LENGTH//4 + i)}C')
+        sys.stdout.write('\u001b[40;1m ')
+        sys.stdout.write(f'\u001b[{str(LENGTH//4 + i)}D')
+        sys.stdout.write(f'\u001b[{str(LENGTH//4 - i)}C')
+        sys.stdout.write('\u001b[40;1m ')
+        sys.stdout.write(f'\u001b[{str(LENGTH//4 - i)}D')
 
         #creates right half
-        sys.stdout.write(u'\u001b[' + str((length//4)*3 + i - 4) + 'C')
-        sys.stdout.write(u'\u001b[40;1m ')
-        sys.stdout.write(u'\u001b[' + str((length//4)*3 + i - 4) + 'D')
-        sys.stdout.write(u'\u001b[' + str((length//4)*3 - i - 4) + 'C')
-        sys.stdout.write(u'\u001b[40;1m ')
+        sys.stdout.write(f'\u001b[{str((LENGTH//4)*3 + i - 4)}C')
+        sys.stdout.write('\u001b[40;1m ')
+        sys.stdout.write(f'\u001b[{str((LENGTH//4)*3 + i - 4)}D')
+        sys.stdout.write(f'\u001b[{str((LENGTH//4)*3 - i - 4)}C')
+        sys.stdout.write('\u001b[40;1m ')
 
         #sets new row
-        sys.stdout.write(u'\u001b[1B')
-        sys.stdout.write(u'\u001b[100D')
+        sys.stdout.write('\u001b[1B')
+        sys.stdout.write('\u001b[100D')
     #resets terminal settings
-    sys.stdout.write(u'\u001b[' + str(width) + 'B')
-    sys.stdout.write(u'\u001b[0m')
+    sys.stdout.write(f'\u001b[{str(WIDTH)}B')
+    sys.stdout.write('\u001b[0m')
 
 for i in range(10):
     pattern()
